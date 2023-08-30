2c3b18bc2cb43299c1aa7e81dec592291d8766c0
tanya <tlathion@icloud.com>
2023-08-30 07:22:59 +0200

    corrections sur new

--- app/models/lens.rb
+++ app/models/lens.rb
@@ -2,4 +2,7 @@ class Lens < ApplicationRecord
   belongs_to :user
   belongs_to :camera
   has_many :bookings
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
