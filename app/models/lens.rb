5c89b1a9ab89325a29ebe14d971e4a96368f5400
Maxime Hmae <max.hmae@gmail.com>
2023-09-03 10:15:20 +0200

    add pgsearch gem and start working on navbar design and behavior

--- app/models/lens.rb
+++ app/models/lens.rb
@@ -8,4 +8,12 @@ class Lens < ApplicationRecord
   validates :name, presence: true
   validates :lens_type, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_synopsis,
+  against: [ :title, :synopsis ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
