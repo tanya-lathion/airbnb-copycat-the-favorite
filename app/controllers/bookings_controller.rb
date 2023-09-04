45c8e419a22269fd0e626ebae6077a338c412238
tanya <tlathion@icloud.com>
2023-09-04 23:03:21 +0200

    errease some unespected spaces between lines

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -44,6 +44,7 @@ class BookingsController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_booking
     @booking = Booking.find(params[:id])
--- app/controllers/lenses_controller.rb
+++ app/controllers/lenses_controller.rb
@@ -47,6 +47,7 @@ class LensesController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_lens
     @lens = Lens.find(params[:id])
